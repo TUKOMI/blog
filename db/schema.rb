97c8974291610afbd456c1d6f404258709e6e7e8
TUKOMI-dev <mryund@gmail.com>
2015-06-03 17:12:02 +0800

    create projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603085454) do
+ActiveRecord::Schema.define(version: 20150603091102) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
     t.string   "pic_url"
     t.string   "email"
     t.string   "skills"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.string   "text"
+    t.string   "pic_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
